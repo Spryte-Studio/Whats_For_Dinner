b33f09a58407fccbfb99e38716f9d74bc536ac11
davidkharbin <david_k_harbin@hotmail.com>
2021-07-10 16:47:29 -0600

    build: initial fridge component test

--- client/src/components/storage/Storage.jsx
+++ client/src/components/storage/Storage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Fridge from './fridgePantry/Fridge.jsx';
+import Pantry from './fridgePantry/Pantry.jsx'
 
 var Storage = () => {
   return (
     <div id='Storage'>
-
+      <Fridge />
+      <Pantry />
     </div>
   );
 };
--- client/src/components/storage/fridgePantry/Fridge.jsx
+++ client/src/components/storage/fridgePantry/Fridge.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 var Fridge = () => {
   return (
     <div id='Fridge'>
-
+      <h1>hello fridge</h1>
     </div>
   );
 };
