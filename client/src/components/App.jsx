27dd634e9f88b8f391a96e1924682cfdcf40bf8b
James Moore <jameswmoore99@gmail.com>
2021-07-10 15:22:34 -0600

    Restored App

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -11,9 +11,44 @@ import Recipes from './recipes/Recipes.jsx';
 
 var App = () => {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/fridge'>Fridge</Link>
+          </li>
+          <li>
+            <Link to='/recipes'>Recipes</Link>
+          </li>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+          <li>
+            <Link to='/signup'>Sign Up</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+          <Route path='/storage' exact>
+            <Storage />
+          </Route>
+          <Route path='/recipes' exact>
+            <Recipes />
+          </Route>
+          <Route path='/login' exact>
+            <Login />
+          </Route>
+          <Route path='/signup' exact>
+            <SignUp />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 };
 
