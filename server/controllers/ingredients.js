8ef3feac7b02e4cf198a030fe262eae0d493400f
Mikka Tully <mikka.tully@gmail.com>
2021-07-12 15:14:40 -0600

    Outline query params for axios request to api server

--- server/controllers/ingredients.js
+++ server/controllers/ingredients.js
@@ -6,21 +6,29 @@ const axios = require('axios');
 const router = express.Router();
 const models = require('../models');
 
+const autoCompleteURL = 'https://api.edamam.com/auto-complete';
 
-//router.get('/', (req, res) => {})
-// router.get('/:productId', (req, res) => {
-//   // console.log('get//products/:product_id');
-//   axios.get(`${BaseUrl}/products/${req.params.productId}`, {
-//     headers: { 'Authorization': GITHUB_API_KEY },
-//   })
-//     .then((response) => {
-//       // console.log('this is response inside get request',response);
-//       // console.log('productMain get response.data: ', response.data);
-//       res.status(200).send(response.data);
-//     })
-//     .catch((err) => {
-//       console.log('S: productMain get/:productId err: ', err);
-//       res.status(500).send(err);
-//     });
-// });
+// router.get('/', (req, res) => {})
+
+router.get('/:ingredientName', (req, res) => {
+  // console.log('get//products/:product_id');
+  axios.get(`${autoCompleteURL}`, {
+    query: {
+      'app_id': edamam_ingredientSearch_app_id,
+      'app_key': edamam_ingredientSearch_app_key,
+      'q': req.query.ingredientName
+    },
+  })
+    .then((response) => {
+      // console.log('this is response inside get request',response);
+      // console.log('productMain get response.data: ', response.data);
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      console.log('S: productMain get/:productId err: ', err);
+      res.status(500).send(err);
+    });
+});
+
+
 module.exports = router;
