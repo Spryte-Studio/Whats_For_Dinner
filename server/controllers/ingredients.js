a6b74da5b26ed884c14c6f7ce271a3ea64cddd1b
Mikka Tully <mikka.tully@gmail.com>
2021-07-12 15:56:12 -0600

    API req working via postman through to our localhost server - rerouting request with specified params to return all possible autocomplete ingredients

--- server/controllers/ingredients.js
+++ server/controllers/ingredients.js
@@ -15,21 +15,20 @@ const {edamam_ingredientSearch_app_id, edamam_ingredientSearch_app_key} = proces
 // router.get('/', (req, res) => {})
 
 router.get('/:ingredientName', (req, res) => {
-  // console.log('get//products/:product_id');
+  // console.log('req at ingredients api', req.params);
   axios.get(`${autoCompleteURL}`, {
-    query: {
+    params: {
       'app_id': edamam_ingredientSearch_app_id,
       'app_key': edamam_ingredientSearch_app_key,
-      'q': req.query.ingredientName
-    },
+      'q': req.params.ingredientName
+    }
   })
     .then((response) => {
-      // console.log('this is response inside get request',response);
-      // console.log('productMain get response.data: ', response.data);
+      // console.log('this is response inside get request',response.data);
       res.status(200).send(response.data);
     })
     .catch((err) => {
-      console.log('S: productMain get/:productId err: ', err);
+      console.log('S: ingredients get/:ingredientName err: ', err);
       res.status(500).send(err);
     });
 });
