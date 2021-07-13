2d6acb3cc1e6a7ef71459382c88eec2653d805d2
mrprov12 <mrprov@mac.com>
2021-07-13 15:36:33 -0600

    syncing

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 const { client_secret } = process.env;
 
-const controllers = require('./controllers')
+const controllers = require('./controllers');
 
 const PORT = 3000;
 const app = express();
