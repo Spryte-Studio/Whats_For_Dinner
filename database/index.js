3b605fcfb13cb62ea0fa4435e4bf5c7054fa6455
davidkharbin <david_k_harbin@hotmail.com>
2021-07-13 18:27:17 -0600

    build:  connected to cloud database server

--- database/index.js
+++ database/index.js
@@ -7,7 +7,6 @@ const dbConfig = { user, host, database, password, port};
 
 const pool = new Pool(dbConfig);
 
-
 pool.connect((err) => {
   if (err) {
     console.log(err,'error connecting to database', err)
