f05a35a4875727968a8a314f1f8b80f88e9dca00
davidkharbin <david_k_harbin@hotmail.com>
2021-07-13 21:40:44 -0600

    build:  db recreation q/unique keys

--- server/controllers/inventory.js
+++ server/controllers/inventory.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const cors = require('cors');
 const express = require('express');
 const axios = require('axios');
-
 const router = express.Router();
 const models = require('../models');
+
+const dbURL =
+
+
 
 
 module.exports = router;
