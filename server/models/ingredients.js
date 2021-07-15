d00f7b7655e6dc283de7b4273e9f98e64aa24529
James Moore <jameswmoore99@gmail.com>
2021-07-15 16:27:41 -0600

    Done with mikka changes

--- server/models/ingredients.js
+++ server/models/ingredients.js
@@ -1,34 +1,77 @@
 const db = require('../../database');
 
-const postIngredient = function (
+// const postIngredient = function (
+//   ingredients, perishable, UID, callback) {
+
+//   console.log('ingredients in model', ingredients);
+
+//   const insertIngredientToInventory =
+//     `INSERT INTO inventory (name, perishable) VALUES ($1, $2) RETURNING ID;`;
+
+
+//   db.query('SELECT id FROM users WHERE auth_code = $1;', [UID])
+//     .then((userID) => {
+//       ingredients.map((ingredientName) => (
+//         db.query(insertIngredientToInventory, [ingredientName, perishable])
+//           .then((results) => {
+//             const inventory_id = results.rows[0].id;
+//             const insertIngredientForUser =
+//               `INSERT INTO users_inventory (user_id, inventory_id) VALUES ($1, $2);`;
+
+//             db.query(insertIngredientForUser, [userID.rows[0].id, inventory_id])
+//               .then(() => {
+//                 callback(null, 'successfully added ingredients! :)')
+//               })
+//               .catch((err) => {
+//                 callback(err, null);
+//               });
+//           })
+//           .catch((err) => {
+//             callback(err, null);
+//           })
+//       ))
+//     })
+//     .catch((err) => {
+//       callback(err, null);
+//     })
+// };
+
+const postIngredient = function(
   ingredients, perishable, UID, callback) {
-
   console.log('ingredients in model', ingredients);
-
   const insertIngredientToInventory =
     `INSERT INTO inventory (name, perishable) VALUES ($1, $2) RETURNING ID;`;
-
-
   db.query('SELECT id FROM users WHERE auth_code = $1;', [UID])
     .then((userID) => {
       ingredients.map((ingredientName) => (
-        db.query(insertIngredientToInventory, [ingredientName, perishable])
-          .then((results) => {
-            const inventory_id = results.rows[0].id;
-            const insertIngredientForUser =
-              `INSERT INTO users_inventory (user_id, inventory_id) VALUES ($1, $2);`;
-
-            db.query(insertIngredientForUser, [userID.rows[0].id, inventory_id])
-              .then(() => {
-                callback(null, 'successfully added ingredients! :)')
-              })
-              .catch((err) => {
-                callback(err, null);
-              });
-          })
-          .catch((err) => {
-            callback(err, null);
-          })
+        db.query('SELECT id FROM inventory WHERE name=$1', [ingredientName])
+        .then((results) => {
+          const inventory_id = results.rows[0].id;
+          const insertIngredientForUser =
+            `INSERT INTO users_inventory (user_id, inventory_id) VALUES ($1, $2);`;
+          db.query(insertIngredientForUser, [userID.rows[0].id, inventory_id])
+            .then(() => {
+              callback(null, 'successfully added ingredients! :)')
+            })
+            .catch((err) => {
+              callback(err, null);
+            });
+        })
+        .catch((err) => {
+          db.query(insertIngredientToInventory, [ingredientName, perishable])
+            .then((results) => {
+              const inventory_id = results.rows[0].id;
+              const insertIngredientForUser =
+                `INSERT INTO users_inventory (user_id, inventory_id) VALUES ($1, $2);`;
+              db.query(insertIngredientForUser, [userID.rows[0].id, inventory_id])
+                .then(() => {
+                  callback(null, 'successfully added ingredients! :)')
+                })
+                .catch((err) => {
+                  callback(err, null);
+                });
+            })
+        })
       ))
     })
     .catch((err) => {
